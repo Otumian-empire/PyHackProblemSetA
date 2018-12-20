a5fe1fa3123b6555f39ad7323120e92494140dfd
otumian <popecan1000@gmail.com>
2018-12-20 07:06:21 +0000

    problem 10 solved

---
+++ first1000fact_M3n6.py
@@ -0,0 +1,18 @@
+# 10 - Find the first 1000 factorial numbers that are both multiples of 3 and 6
+
+def fact(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact(n-1)
+
+fact_number = aux_number = 0
+
+while True:
+    if fact_number == 1000:
+        break
+    elif (fact(fact_number + aux_number) % 3 == 0) and (fact(fact_number + aux_number) % 6 == 0):
+        print(fact(fact_number + aux_number))
+        fact_number += 1
+    else:
+        aux_number += 1
